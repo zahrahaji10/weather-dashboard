babaf2c3bcf46405cb566672db31e5915546cd0a
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-05-28 10:54:48 +0100

    added styling to alert div

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
-//~ UTILITY FUNCTIONS
+// ~ DECLARATIONS
+const recentSearchesContainer = $("#recent-searches");
 
+//~ UTILITY FUNCTIONS
 // Local Storage (LS) function
 const readFromLocalStorage = (key, defaultValue) => {
   // get from LS using key name
@@ -15,16 +17,28 @@ const readFromLocalStorage = (key, defaultValue) => {
   }
 };
 
-// first fn called on document load
-const onLoad = () => {
-  //target the parent element for the recent searches in HTML
-  const RecentSearchesDiv = $("#recent-searches");
-
-  console.log("hi");
-
+// second - fn to render recent search on page after load
+const renderRecentSearch = () => {
+  //target recent searches in HTML
   // get data from LS after page load - parse data to LS
   const recentSearches = readFromLocalStorage("recentSearches", []);
-  console.log(recentSearches);
+  // conditional statement for
+  if (recentSearches.length) {
+    // if there is recent search display searches
+  } else {
+    // if there are no searches render an alert
+    const noSearchAlert = `<div class="alert alert-dark" role="alert">
+        You have no search history
+    </div>`;
+    //append alert to parent div (recent search container)
+    $("#recent-searches").append(noSearchAlert);
+  }
+};
+
+// first - \fn called on document load
+const onLoad = () => {
+  // calling fn to render searches on page
+  renderRecentSearch();
 };
 
 // add on ready event listener for document using jquery
